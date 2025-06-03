72737e6b222678f9f060429178f00c866fbcf982
JoeRodas <166660958+JoeRodas@users.noreply.github.com>
2025-06-03 16:06:33 -0400

    Implement full piece movement rules

---
+++ Sources/CasaAjedrez/Game.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+public enum GameError: Error {
+    case invalidMove
+}
+
+public struct Game {
+    public private(set) var board: Board
+    public private(set) var currentTurn: PieceColor
+
+    public init() {
+        self.board = Board()
+        self.currentTurn = .white
+    }
+
+    public mutating func applyMove(from: (Int, Int), to: (Int, Int)) throws {
+        guard let piece = board[from.0, from.1], piece.color == currentTurn,
+              board.isValidMove(for: piece, from: from, to: to) else {
+            throw GameError.invalidMove
+        }
+
+        board[from.0, from.1] = nil
+        board[to.0, to.1] = piece
+        currentTurn = currentTurn == .white ? .black : .white
+    }
+}
