1b4fc36a030cdeec821bfbdb51325976911c5d74
JoeRodas <166660958+JoeRodas@users.noreply.github.com>
2025-06-04 13:00:53 -0400

    Update SwiftUIBoard.swift
    Fixed the same bug only available in ios 13.0 or later for the WelcomeView and GameView. 
      
      - By adding the following 
           - @available(iOS 13.0, *)

--- Sources/CasaAjedrez/SwiftUIBoard.swift
+++ Sources/CasaAjedrez/SwiftUIBoard.swift
@@ -77,6 +77,7 @@ public struct BoardView: View {
     }
 }
 
+@available(iOS 13.0, *)
 public struct WelcomeView: View {
     @State private var selectedColor: PieceColor? = nil
 
@@ -99,6 +100,7 @@ public struct WelcomeView: View {
     }
 }
 
+@available(iOS 13.0, *)
 private struct GameScreen: View {
     @StateObject var viewModel: GameViewModel
 
